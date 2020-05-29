7e97f0351569e803e06c0338254a34ff5a1a9f6f
alen <alen@gamesci.com.cn>
2020-05-29 19:29:44 +0800

    kmp

---
+++ algorithm/kmp.c
@@ -0,0 +1,128 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//kmp o(m+n)
+//在字符集中查找关键字起始索引
+//abcdefaattssafd   find  tts
+void make_next(const char *pattern, int *next)
+{
+    int q, k;
+    int m = strlen(pattern);
+
+    next[0] = 0;
+    k = 0;
+    for (q = 1; q < m; q++)
+    {
+        while (k > 0 && pattern[q] != pattern[k])
+        {
+            k = next[k - 1];
+        }
+
+        if (pattern[q] == pattern[k])
+        {
+            k++;
+        }
+
+        next[q] = k;
+    }
+}
+
+int kmp(const char *text, const char *pattern, int *next)
+{
+    //check except
+
+    int n = strlen(text);
+    int m = strlen(pattern);
+
+    make_next(pattern, next);
+
+    int i, q=0;
+    for (i = 0; i < n; i++)
+    {
+        while (q > 0 && pattern[q] != text[i])
+        {
+            q = next[q - 1];
+        }
+
+        if (pattern[q] == text[i])
+        {
+            q++;
+        }
+
+        if (q == m)
+        {
+            break;
+        }
+    }
+
+    if (q==m){
+        return i - q + 1;
+    }
+    return -1;
+}
+
+//https://www.cnblogs.com/zhangtianq/p/5839909.html
+void make_next2(const char *pattern, int *next){
+    int m = strlen(pattern);
+    next[0] = -1;
+    int k = -1, j = 0;
+
+    //前缀和后缀的公共最大子串个数 右移动一位了
+    //example next 0   0   0   1   2   0  ->   next -1   0   0   0   1   2
+    while( j < m -1){
+        if (k == -1 || pattern[j] == pattern[k]){
+            ++k;
+            ++j;
+            next[j] = k;
+        }
+        else {
+            k = next[k];
+        }
+    }
+    return;
+}
+int kmp2(const char *text, const char *pattern, int *next){
+
+    make_next2(pattern, next);
+    
+    int i = 0, j = 0;
+    int n = strlen(text);
+    int m = strlen(pattern);
+
+    while( i < n && j < m){
+        if (j == -1 || text[i] == pattern[j]){
+            i++;
+            j++;
+        }
+        else{
+            j = next[j];//pattern向右移动 m - next[j]
+        }
+    }
+    if (j == m){
+        return i - j;
+    }
+    return -1;
+}
+
+
+
+int main(void){
+    int next[20] = {0};
+    const char *text = "ababxbababababcdababcabddcadfdsss";
+    const char *pattern = "abcabd";
+
+    printf("text:%d\n", strlen(text));
+
+    // int idx = kmp(text, pattern, next);//如果pattern不存在将返回一个不存在的索引
+    int idx = kmp2(text, pattern, next);
+    printf("match pattern : %d\n", idx);
+
+    for (int i = 0; i < strlen(pattern); i++)
+    {
+        printf("%4d", next[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
--- algorithm/sort.c
+++ algorithm/sort.c
@@ -6,6 +6,7 @@ void bubble_sort(int *pdata, int ilength)
 {
     int itemp = 0;
     for(int i = 0; i < ilength; i++){
+        
         for (int j = 0; j < ilength-1-i; j++){
 
             if (pdata[j] > pdata[j+1]){
@@ -23,7 +24,9 @@ void bubble_sort2(int *pdata, int ilength)
     int itemp=0;
     // bool is_change = false;
     char is_exchange = 0;
+    
     for (int i = 0; i<ilength; i++){
+        
         is_exchange = 0;
         for(int j=ilength-1; j>0; j--){
 
@@ -34,6 +37,7 @@ void bubble_sort2(int *pdata, int ilength)
                 is_exchange = 1;
             }
         }
+        
         if(!is_exchange){
             break;
         }
@@ -49,6 +53,7 @@ void insert_sort(int *pdata, int ilength)
     if(!pdata) return;
     int itemp = 0;
     int i = 0, j = 0;
+    
     for (i=1; i<ilength; i++){
 
         itemp = pdata[i];
@@ -63,6 +68,7 @@ void insert_sort(int *pdata, int ilength)
 
 //shell sort
 //o(n^1.3)  ~   o(n*n)
+//�������������һ����������
 void shell_sort(int *pdata, int ilength)
 {
     if (!pdata) return;
@@ -74,9 +80,9 @@ void shell_sort(int *pdata, int ilength)
         for(i = step; i < ilength; i++){
             itemp=pdata[i];
             for(j = i-step; j >= 0 && itemp < pdata[j]; j -= step){
-                pdata[j + step] = pdata[j];
+                pdata[j + step] = pdata[j];// ����Ķ������ݺ���
             }
-            pdata[j + step] = itemp;
+            pdata[j + step] = itemp;//��������
         }
     }
     return;
@@ -86,6 +92,9 @@ void shell_sort(int *pdata, int ilength)
 //merge sort
 void merge_sort_i(int *pdata, int *ptemp, int istart, int imiddle, int iend)
 {
+    //static int call_count=0; // call count
+    //printf("%4d", call_count++);
+
     int i = istart, j = imiddle+1, k=istart;
     while(i <= imiddle && j <= iend){
         if(pdata[i] >= pdata[j]){
@@ -99,6 +108,7 @@ void merge_sort_i(int *pdata, int *ptemp, int istart, int imiddle, int iend)
     while(i <= imiddle){
         ptemp[k++] = pdata[i++];
     }
+    
     while(j <= iend){
         ptemp[k++] = pdata[j++];
     }
@@ -112,7 +122,9 @@ void merge_sort_i(int *pdata, int *ptemp, int istart, int imiddle, int iend)
 void merge_sort(int *pdata, int *ptemp, int istart, int iend)
 {
     if(!pdata || !ptemp) return;
+    
     int imiddle = istart + (iend-istart) / 2;
+    
     if(istart < iend){
         merge_sort(pdata, ptemp, istart, imiddle);
         merge_sort(pdata, ptemp, imiddle+1, iend);
@@ -123,10 +135,72 @@ void merge_sort(int *pdata, int *ptemp, int istart, int iend)
 }
 
 
+//ѡ������
+//o(n*n)
+void select_sort(int *pdata, int ilength)
+{
+    int max_elm_idx = 0, itemp=0;
+    int i=0, j=0;
+    
+    for(i=0; i<ilength; i++){//ִ������ÿһ���ҳ����ķ����������/����
+
+        max_elm_idx = ilength-1-i;
+        for(j=0; j<ilength-i; j++){
+            if(pdata[max_elm_idx] < pdata[j]){
+                max_elm_idx = j;
+            }
+        }
+
+        if(max_elm_idx != ilength-1-i){
+            itemp = pdata[max_elm_idx];
+            pdata[max_elm_idx] = pdata[ilength-1-i] ;
+            pdata[ilength-1-i] = itemp;
+        }
+    }
+    return;
+}
+
+
+//��������
+//o(nlogn)
+void quick_sort_i(int *data, int left, int right){
+
+    if(right <= left) return;
+
+    int i = left, j = right;
+    int key = data[left];
+    
+    while(i != j){
+
+        while(i < j && data[j] >= key){
+            j--;
+        }
+        data[i] = data[j];
+
+        while(i < j && data[i] <= key){
+            i++;
+        }
+        data[j] = data[i];
+    }
+    data[i] =  key;
+
+    quick_sort_i(data, left, i-1); // i �Ѿ���λ�˲���Ҫ����
+    quick_sort_i(data, i+1, right);
+    
+    return;
+}
+void quick_sort(int *pdata, int ilength)
+{
+    quick_sort_i(pdata, 0, ilength-1);
+    return;
+}
+
+
 
 int main(void)
 {
     int data[] = {17, 99, 45, 33, 26, 87, 32, 75, 32, 10};
+    //int data[]= {2,4,3,1};
     int length = sizeof(data)/sizeof(int);
 #if 0
     //bubble_sort(data, length);
@@ -135,12 +209,18 @@ int main(void)
     insert_sort(data, length);
 #elseif 0
     shell_sort(data, length);
-#else
+#elseif 0
     int temp[length] = {0};
     merge_sort(data, temp, 0, length-1);
+#elseif 0
+    select_sort(data, length);
+#elseif 0
+    quick_sort(data, length);
+
+    
 #endif
 
-
+    printf("\n");
     for (int i=0; i<length; i++){
         printf("%4d", data[i]);
     }
