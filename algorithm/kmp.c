c5329756769da2df8fe6acfc94d75f924eb522b7
alen <alen@gamesci.com.cn>
2020-05-29 23:16:44 +0800

    kmp

--- algorithm/kmp.c
+++ algorithm/kmp.c
@@ -70,7 +70,7 @@ void make_next2(const char *pattern, int *next){
 
     //前缀和后缀的公共最大子串个数 右移动一位了
     //example next 0   0   0   1   2   0  ->   next -1   0   0   0   1   2
-    while( j < m -1){
+    while( j < m - 1){
         if (k == -1 || pattern[j] == pattern[k]){
             ++k;
             ++j;
@@ -86,11 +86,13 @@ int kmp2(const char *text, const char *pattern, int *next){
 
     make_next2(pattern, next);
     
-    int i = 0, j = 0;
+    int i = 0, j = 0;// i是 text的索引, j 是pattern索引
     int n = strlen(text);
     int m = strlen(pattern);
 
     while( i < n && j < m){
+
+        // next 数组中最小的是-1
         if (j == -1 || text[i] == pattern[j]){
             i++;
             j++;
@@ -99,6 +101,7 @@ int kmp2(const char *text, const char *pattern, int *next){
             j = next[j];//pattern向右移动 m - next[j]
         }
     }
+
     if (j == m){
         return i - j;
     }
